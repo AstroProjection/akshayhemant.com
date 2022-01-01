b045ad7f623c64517e09b490d030301c433c0c8f
Akshay Hemant <akshayhemant@hotmail.com>
2022-01-01 19:50:26 +0530

    Updated npm packages:  - Configured webpack  - Added image loader

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -1,9 +1,11 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js', /// relative path to entry of proj
+  mode: process.env.NODE_ENV || "development",
+  entry: "./src/index.js", /// relative path to entry of proj
   output: {
-    filename: 'main.js', /// name of the output for js
-    path: require('path').resolve(__dirname, 'dist'), /// absolute path of the folder
+    filename: "main.js", /// name of the output for js
+    path: path.resolve(__dirname, "dist"), /// absolute path of the folder
   },
   module: {
     rules: [
@@ -12,21 +14,34 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           // without additional settings, it will reference .babelrc
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader', // 2.injects file to dom
-          'css-loader', // 1.turns css into js
+          "style-loader", // 2.injects file to dom
+          "css-loader", // 1.turns css into js
         ], // the order of the loaders is IMPORTANT!
+      },
+      {
+        test: /\.jpg$/,
+        use: ["file-loader"],
       },
     ],
   },
   /// source map is to video the original file as typed
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
-    contentBase: './dist',
+    liveReload: true,
+    hot: true,
+    port: 9000,
+    static: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html",
+      filename: "index.html",
+    }),
+  ],
 };
